307fc6df9553bf43cdb7874574e80d7f5f2db509
tanjima12 <tanjimaakter005@gmail.com>
2023-11-08 06:27:06 +0600

    delete

--- index.js
+++ index.js
@@ -224,7 +224,12 @@ async function run() {
       const result = await NewsCollection.updateOne(filter, nameBlog, options);
       res.send(result);
     });
-
+    app.delete("/wishBlog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await WishListCollection.deleteOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
